9749982abde972a957285bc6b9d44cc60381b289
indexly-project <indexlyproject@gmail.com>
2026-07-09 14:40:31 +0000

    Add analytics tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,6 +89,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name="google-site-verification" content="Um4CIb6wu_DDi2IfiWFVAEd2Ddkx8hjWSj3ZMYQp14Y" />
         <meta name="indexed-by" content="Indexly" data-indexly-id="idx-gp91hupe1wn2473u" />
+        <script src="https://indexly-snowy.vercel.app/track.js" data-site-id="idx-pz8c1ca8bn3j9xo6" defer></script>
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaWebApp) }} />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaOrg) }} />
       </head>
